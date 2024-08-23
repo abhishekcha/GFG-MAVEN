9e26d6349cc7e9dc650bcee464a7f00cb8ff8d85
abhishek chaudhary <abhishek0221997@gmail.com>
2024-08-23 14:04:53 +0530

    show() function added

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -17,6 +17,10 @@ public class MyCalc {
 	{
 		return a/b;
 	}
+	public void show()
+	{
+		System.out.println("GFG show Function");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyCalc calc=new MyCalc();
@@ -24,6 +28,7 @@ public class MyCalc {
 		System.out.println("diff is :"+calc.diff(244, 40));
 		System.out.println("multiplication is :"+calc.mul(24, 20));
 		System.out.println("division is :"+calc.divi(24, 20));
+		calc.show();
 	}
 
 }
